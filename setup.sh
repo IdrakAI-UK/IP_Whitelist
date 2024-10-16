388c63a1689f2a072e6bf522b78f9ec7f3dc0b93
Umair Shah <97160480+UmairShah7677@users.noreply.github.com>
2024-10-16 20:22:51 +0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Move the scripts to /root directory
+echo "Starting Setup..."
+
+
+# Make the scripts executable
+chmod +x /root/ufw_firewall.sh
+chmod +x /root/fetch_ip.sh
+
+# Set up cron jobs for fetch_ip.sh and ufw_firewall.sh
+echo "Setting up cron jobs..."
+
+# Remove any existing cron jobs related to fetch_ip.sh and ufw_firewall.sh
+crontab -l | grep -v "fetch_ip.sh" | grep -v "ufw_firewall.sh" | crontab -
+
+# Add cron job for fetch_ip.sh every 5 minutes
+(crontab -l; echo "*/5 * * * * /root/fetch_ip.sh") | crontab -
+
+# Add cron job for ufw_firewall.sh every 5 minutes
+(crontab -l; echo "*/5 * * * * /root/ufw_firewall.sh") | crontab -
+
+echo "Cron jobs set up successfully!"
+
+echo "Firewall set up successfully!"
